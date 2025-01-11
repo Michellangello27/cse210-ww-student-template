e18eb874c46e163a157a6cda1c753edde80fa5fc
Michellangello27 <cor24023@byui.edu>
2025-01-11 10:28:28 -0500

    Exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,9 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
+
         Console.WriteLine("Hello Prep1 World!");
-=======
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
@@ -14,7 +13,5 @@ class Program
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
-
->>>>>>> 26ec8c2 (Exercise 1)
     }
 }
